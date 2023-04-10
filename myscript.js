a9557c1a46d3f171c744bbb2d9d381607a777bd0
LaMar Fisher <lsfisher503@gmail.com>
2023-04-09 19:32:59 -0700

    added form html/css, adjusted home img

--- myscript.js
+++ myscript.js
@@ -1,9 +1,30 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "Added Javascript"
     } 
-    
 
-      const myButton = document.getElementById("myButton");
-      myButton.addEventListener("click", function() {
-        alert("I am learning to use AI to assist me in understanding js");
+
+      const form = document.querySelector("#form")
+      const submitButton = document.querySelector("#submit")
+      const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+   
+      form.addEventListener('submit', e => {
+        console.log('Form submitted');
+        submitButton.disabled = true
+        e.preventDefault()
+        let requestBody = new FormData(form)
+        fetch(scriptURL, { method: 'POST', body: requestBody})
+          .then(response => {
+            console.log('Response received');
+            if (response.ok) {
+              alert('Success!');  
+            } else {
+              alert('Error!');
+            }
+             submitButton.disabled = false
+          })
+          .catch(error => {
+            console.log('Error:', error);
+            alert('Error!');
+            submitButton.disabled = false;
+          });
       });
